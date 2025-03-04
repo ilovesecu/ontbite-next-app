8ea54a3e4e46c4ce6d019388d76481402443baf0
정승주 <ilovesecu@gmail.com>
2025-03-04 10:50:37 +0900

    3.5) 리액트 서버 컴포넌트 주의사항 마무리

--- src/app/(with-searchbar)/client-component.tsx
+++ src/app/(with-searchbar)/client-component.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import ServerComponent from "@/app/(with-searchbar)/server-component";
+import {ReactNode} from "react";
 
-export default function ClientComponent(){
+export default function ClientComponent({children}:{children:ReactNode}){
     console.log('Client Component')
     return (
         <div>
-            <ServerComponent></ServerComponent>
+            {children}
         </div>
     )
 }
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ClientComponent from "@/app/(with-searchbar)/client-component";
+import ServerComponent from "@/app/(with-searchbar)/server-component";
 
 export default function Home() {
   return (
     <>
       <div className={styles.page}>
           INDEX 페이지
-          <ClientComponent></ClientComponent>
+          <ClientComponent>
+              <ServerComponent/>
+          </ClientComponent>
       </div>
     </>
   );
