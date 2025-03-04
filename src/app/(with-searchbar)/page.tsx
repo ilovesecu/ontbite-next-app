bf7dc23dbf4243edbe21f7792a7dcff334f917e3
정승주 <ilovesecu@gmail.com>
2025-03-04 16:15:14 +0900

    4.1) 앱 라우터의 데이터 페칭

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -4,6 +4,42 @@ import ClientComponent from "@/components/client-component";
 import ServerComponent from "@/components/server-component";
 import books from "@/mock/books.json";
 import BookItem from "@/components/bookItem";
+import {BookData} from "@/types";
+
+async function AllBooks(){
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+    if(!response.ok){
+        return <div>오류가 발생했습니다.</div>
+    }
+    const allBooks :BookData[] = await response.json();
+    return (
+        <div>
+            {
+                allBooks.map(book => {
+                    return (<BookItem key={book.id} {...book}/>)
+                })
+            }
+        </div>
+    )
+}
+
+async function RecBooks(){
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+    if(!response.ok){
+        return <div>오류가 발생했습니다.</div>;
+    }
+    const recBooks:BookData[] = await response.json();
+
+    return (
+        <div>
+            {
+                recBooks.map(book=>{
+                    return (<BookItem key={book.id} {...book}/>)
+                })
+            }
+        </div>
+    )
+}
 
 export default function Home() {
   return (
@@ -11,19 +47,11 @@ export default function Home() {
       <div className={styles.container}>
           <section>
               <h3>지금 추천하는 도서</h3>
-              {
-                  books.map(book => {
-                      return (<BookItem key={book.id} {...book}/>)
-                  })
-              }
+              <RecBooks/>
           </section>
           <section>
               <h3>등록된 모든 도서</h3>
-              {
-                  books.map(book => {
-                      return (<BookItem key={book.id} {...book}/>)
-                  })
-              }
+              <AllBooks />
           </section>
       </div>
     </>
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,16 @@
-import ClientComponent from "@/components/client-component";
-import books from "@/mock/books.json";
 import BookItem from "@/components/bookItem";
+import {BookData} from "@/types";
 export default async function Page({searchParams}
                                        :
                                        { searchParams: Promise<{ q: string }> }) {
     const {q} = await searchParams;
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`);
+    if(!response.ok){
+        return <div>오류가 발생했습니다.</div>
+    }
+
+    const books:BookData[] = await response.json();
+
     return (
         <div>
             {
