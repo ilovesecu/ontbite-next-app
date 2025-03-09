8c2b286afe400129959149489a4e46859cb13851
ilovesecu <ilovesecu@gmail.com>
2025-03-10 00:49:25 +0900

    7.6) 클라이언트 컴포넌트에서의 서버액션

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,14 +1,18 @@
 'use server'
 import {revalidatePath, revalidateTag} from "next/cache";
+import {delay} from "@/util/delay";
 
-export async function createReviewAction(formData:FormData){
-
+export async function createReviewAction(_:any, formData:FormData){
+    await delay(2000);
     console.log('server action called');
     const content = formData.get('content')?.toString();
     const author = formData.get('author')?.toString();
     const bookId = formData.get('bookId')?.toString();
     if(!content || !author || !bookId){
-        return ;
+        return {
+            status: false,
+            error: "리뷰 내용과 작성자를 입력해주세요",
+        };
     }
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,{
@@ -16,10 +20,21 @@ export async function createReviewAction(formData:FormData){
             body: JSON.stringify({content,author,bookId})
         })
         console.log(response.status);
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
         //revalidatePath(`/book/${bookId}`); //NEXT SERVER가 자동으로 인수로 전달한 경로의 페이지를 재검증함.
         revalidateTag(`review-${bookId}`);
+        return {
+            status:true,
+            error:""
+        }
     }catch (e){
         console.error(e);
+        return{
+            status:false,
+            error:`리뷰 저장에 실패했습니다. ${e}`
+        }
     }
 
 }
--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,14 +1,27 @@
+'use client';
+
 import {createReviewAction} from "@/actions/create-review.action";
 import style from './reivew-editor.module.css';
+import {useActionState, useEffect} from "react";
 
 export default function ReviewEditor({bookId}:{bookId:string}) {
+    const [state,formAction, isPending] = useActionState(createReviewAction, null);
+
+    useEffect(() => {
+        if(state && !state.status){
+            alert(state.error);
+        }
+    }, [state]);
+
     return <section>
-        <form className={style.form_container} action={createReviewAction}>
+        <form className={style.form_container} action={formAction}>
             <input name="bookId" type="hidden" value={bookId} readOnly/>
-            <textarea placeholder="리뷰 내용" name="content" required/>
+            <textarea placeholder="리뷰 내용" name="content" required disabled={isPending}/>
             <div className={style.submit_container}>
-                <input placeholder="작성자" name="author" required/>
-                <button type="submit">작성하기</button>
+                <input placeholder="작성자" name="author" required disabled={isPending}/>
+                <button type="submit" disabled={isPending}>{
+                    isPending ? "..." : "작성하기"
+                }</button>
             </div>
 
         </form>
